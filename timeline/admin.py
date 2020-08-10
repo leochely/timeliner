0d07cf745f3dc5baa42baa834e60cb62a1fddb26
unknown <leo.chely@live.fr>
2020-08-10 12:56:06 -0600

    Admin site updated

--- timeline/admin.py
+++ timeline/admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from django.contrib.auth.models import User, Group
 
-from .models import Evenement, Personnage
+from .models import Evenement, Personnage, Categorie
 
 # Register your models here.
-admin.site.register(Evenement)
-admin.site.register(Personnage)
 admin.site.unregister(User)
 admin.site.unregister(Group)
+
+admin.site.register(Evenement)
+admin.site.register(Personnage)
+admin.site.register(Categorie)
 
 admin.site.site_url = "/timeline"
