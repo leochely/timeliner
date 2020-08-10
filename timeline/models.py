2e54e60e26bc0c5f3bba24e49137c9ac897c517b
unknown <leo.chely@live.fr>
2020-08-10 12:50:29 -0600

    Categorie field added

--- timeline/models.py
+++ timeline/models.py
@@ -6,11 +6,18 @@ class Evenement(models.Model):
     date = models.DateField()
     description = models.CharField(max_length=100)
     personnages = models.ManyToManyField("Personnage")
+    categorie = models.ManyToManyField("Categorie", blank=True)
 
     def __str__(self):
         return self.name
 
 class Personnage(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Categorie(models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
