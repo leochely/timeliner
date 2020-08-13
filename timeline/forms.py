1433a08a2c292acb8e31e328ce0f146d2fb600bb
unknown <leo.chely@live.fr>
2020-08-13 12:28:30 -0600

    Default dates for the filters

--- timeline/forms.py
+++ timeline/forms.py
@@ -5,5 +5,5 @@ from bootstrap_datepicker_plus import DatePickerInput
 class SearchForm(forms.Form):
     personnages = forms.ModelMultipleChoiceField(queryset=Personnage.objects.all())
     combined = forms.BooleanField(required=False)
-    date_depart = forms.DateField(required=True, widget=DatePickerInput())
-    date_fin = forms.DateField(required=True, widget=DatePickerInput())
+    date_depart = forms.DateField(required=True, initial=Evenement.objects.order_by('date').first().date, widget=DatePickerInput())
+    date_fin = forms.DateField(required=True, initial=Evenement.objects.order_by('date').last().date, widget=DatePickerInput())
